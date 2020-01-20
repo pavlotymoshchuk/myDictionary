222267ab2b9e57bd4bc0c894c2d4f09edc765366
Павло Тимощук <Pavlo25575@icloud.com>
2020-01-20 18:04:27 +0200

    Initial Commit

---
+++ myDictionary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myDictionary
+//
+//  Created by Павло Тимощук on 20.01.2020.
+//  Copyright © 2020 Павло Тимощук. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ myDictionaryTests/myDictionaryTests.swift
@@ -0,0 +1,34 @@
+//
+//  myDictionaryTests.swift
+//  myDictionaryTests
+//
+//  Created by Павло Тимощук on 20.01.2020.
+//  Copyright © 2020 Павло Тимощук. All rights reserved.
+//
+
+import XCTest
+@testable import myDictionary
+
+class myDictionaryTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
